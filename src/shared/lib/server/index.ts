50d3de41665bad68c908ff6a217c58667b6f8dc1
esomi <sweetsom27@gmail.com>
2024-10-08 17:38:51 +0900

    fix(product): fix 'Cannot access before initialization' error

--- src/shared/lib/server/index.ts
+++ src/shared/lib/server/index.ts
@@ -1,5 +1,1 @@
 export {db as __serverDb} from './db.ts'
-export {default as productsMock} from './__mocks__/products.json'
-export {default as ordersMock} from './__mocks__/orders.json'
-export {default as reviewsMock} from './__mocks__/reviews.json'
-export {default as usersMock} from './__mocks__/users.json'
