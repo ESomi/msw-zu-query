623c9597e67c625417d06949f404ed6dc8c7d8ed
esomi <sweetsom27@gmail.com>
2024-10-08 12:00:24 +0900

    feat: add mock data

---
+++ src/shared/lib/server/index.ts
@@ -0,0 +1,4 @@
+export { default as productsMock } from './__mocks__/products.json'
+export { default as ordersMock } from './__mocks__/orders.json'
+export { default as reviewsMock } from './__mocks__/reviews.json'
+export { default as usersMock } from './__mocks__/users.json'
